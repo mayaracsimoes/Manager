c2daad53b227851897579e12c1f4a473564b0b4d
mayaracsimoes <mayaracsimoes@gmail.com>
2021-02-28 18:03:58 -0300

    manager

---
+++ src/Conexao/ConexaoBD.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexao;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ConexaoBD {
+
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/manager";
+        String user = "root";
+        String password = "";
+
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (ClassNotFoundException | SQLException e) {
+            return null;
+        }
+    }
+}
